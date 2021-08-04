ebffaf8badcdd30053f65ce117e22a9108f56c4e
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2021-08-04 11:27:53 +0600

    update

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,48 @@
-const user_info = document.getElementById("user_info")
+const user_info = document.getElementById("user_info");
 
 user_info.addEventListener('click',function(e){
     e.preventDefault();
-    document.getElementById("pop_up_nav_links_body").classList.toggle('nav_show')
+    document.getElementById("pop_up_nav_links_body").classList.toggle('nav_show');
+})
+
+const nav_trigger = document.getElementById("nav_trigger");
+
+nav_trigger.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById("toggle").classList.toggle('toggle_nav_show');
 })
 
 
-$(document).ready(function(){
-    $('#nav_trigger').click(function(){
-        $('#toggle').slideToggle(1000);
-    })
+
+let gallery = ["https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500", 
+                "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", 
+                "https://images.pexels.com/photos/2739013/pexels-photo-2739013.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", 
+                "https://images.pexels.com/photos/1329711/pexels-photo-1329711.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+                "https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+                "https://images.pexels.com/photos/2341830/pexels-photo-2341830.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"];
+
+let ImgSelect = document.querySelector("#sliderimg");
+let count = 0;
+
+document.getElementById("next").addEventListener("click", function(){
+    count++;
+    if(count >= gallery.length){
+        
+        count = 0;
+        ImgSelect.src = gallery[count];
+    }
+    else{
+        ImgSelect.src = gallery[count];
+    }
+})
+
+document.getElementById("prev").addEventListener("click", function(){
+    count--;
+    if(count < 0){
+        count = gallery.length - 1;
+        ImgSelect.src = gallery[count];
+    }
+    else{
+        ImgSelect.src = gallery[count];
+    }
 })
